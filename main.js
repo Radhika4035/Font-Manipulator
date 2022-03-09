fee14ebb6f66ddcff267aea6fb4791581b90b971
Radhika4035 <86754890+Radhika4035@users.noreply.github.com>
2022-03-09 17:39:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(540,520);
+
+    canvas = createCanvas(550,510);
+    canvas.position(560,510);
+   
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background('#FFCAED');
+
+    textSize(distance);
+    fill("black");
+    text('Radika',100,100);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized!!');
+}
+
+distance = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        distance = Math.floor(leftWristX - rightWristX) ; 
+        console.log("leftWristX =" + leftWristX + "rightWristX =" + rightWristX + "distance = " + distance);
+    }
+}
+
+
+
